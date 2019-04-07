594a6eda0e6548410877b3a2e245dcc561f59a83
Wentao Shi <wentao771@gmail.com>
2019-04-07 12:55:45 -0500

    added compact and sparse(relu) dense layer as encoder output

--- utils.py
+++ utils.py
@@ -138,6 +138,76 @@ class ConvAutoencoder(nn.Module):
 		x = self.decode(x)
 		return x
 
+class ConvAutoencoder_dense_out(nn.Module):
+	"""
+	Convolutional style autoencoder.
+	Here we implement upsampling by setting stride > 1 in the ConvTranspose2d layers.
+	A dense layer followed by relu is added to the end of encoder and beginning of
+	decoder to force the output features to be compact and sparse.
+	"""
+	def __init__(self, feature_size):
+		super(ConvAutoencoder_dense_out, self).__init__()
+		self.conv1 = nn.Conv2d(3, 16, 3, stride=1, padding=1)
+		self.conv2 = nn.Conv2d(16, 32, 3, stride=1, padding=1)
+		self.conv3 = nn.Conv2d(32, 32, 3, stride=1, padding=1)
+		self.conv4 = nn.Conv2d(32, 16, 3, stride=1, padding=1)
+		self.leaky_relu = nn.LeakyReLU(0.1)
+		self.pool = nn.MaxPool2d(2, 2)
+		self.enc_fc = nn.Linear(4096,feature_size)
+		self.dec_fc = nn.Linear(feature_size,4096)		
+		self.relu = nn.ReLU()
+		self.transconv1 = nn.ConvTranspose2d(16, 16, 4, stride=2, padding=1)
+		self.transconv2 = nn.ConvTranspose2d(16, 32, 4, stride=2, padding=1)
+		self.transconv3 = nn.ConvTranspose2d(32, 32, 4, stride=2, padding=1)
+		self.transconv4 = nn.ConvTranspose2d(32, 3, 4, stride=2, padding=1)
+		self.sigmoid = nn.Sigmoid()
+
+	def flatten(self, x):
+		x = x.view(x.size(0), -1)
+		return x
+
+	def unflatten(self, x):
+		x = torch.reshape(x,(x.size(0),16,16,16))
+		return x
+
+	def encode(self, x):
+		x = self.conv1(x) 		# 16*256*256
+		x = self.leaky_relu(x)  # 16*256*256
+		x = self.pool(x)  		# 16*128*128
+		x = self.conv2(x) 		# 32*128*128
+		x = self.leaky_relu(x)  # 32*128*128
+		x = self.pool(x)  		# 32*64*64
+		x = self.conv3(x) 		# 32*64*64
+		x = self.leaky_relu(x)  # 32*64*64
+		x = self.pool(x)  		# 32*32*32
+		x = self.conv4(x) 		# 16*32*32
+		x = self.leaky_relu(x)  # 16*32*32
+		x = self.pool(x)  		# 16*16*16
+		x = self.flatten(x) 	# 4096
+		x = self.enc_fc(x) 		# feature_size
+		x = self.relu(x) 		# feature_size
+		return x
+
+	def decode(self, x):
+		x = self.dec_fc(x) 		# 4096
+		x = self.leaky_relu(x)  # 4096
+		x = self.unflatten(x)   # 16*16*16
+		x = self.transconv1(x)  # 16*32*32
+		x = self.leaky_relu(x)	# 16*32*32
+		x = self.transconv2(x)  # 32*64*64
+		x = self.leaky_relu(x)	# 32*64*64
+		x = self.transconv3(x)  # 32*128*128
+		x = self.leaky_relu(x)	# 32*128*128
+		x = self.transconv4(x) 	# 3*256*256
+		x = self.sigmoid(x)    	# 3*256*256
+		#print(x.size())
+		return x
+
+	def forward(self, x):
+		x = self.encode(x)
+		x = self.decode(x)
+		return x
+
 class ConvAutoencoder_deeper1(nn.Module):
 	"""
 	Convolutional style autoencoder.
@@ -206,7 +276,6 @@ def train(device, num_epochs, dataloader, model, criterion, optimizer, learningR
 	"""
 	Train the autoencoder
 	"""
-	since = time.time()
 	train_loss_history = []
 
 	# send model to GPU if available
@@ -216,6 +285,7 @@ def train(device, num_epochs, dataloader, model, criterion, optimizer, learningR
 	best_model_wts = copy.deepcopy(model.state_dict())
 	best_loss = float("inf") # initial best loss
 	for epoch in range(num_epochs):
+		since = time.time()
 		print(' ')
 		print('Epoch {}/{}'.format(epoch+1, num_epochs))
 		print('-' * 15)
@@ -223,7 +293,7 @@ def train(device, num_epochs, dataloader, model, criterion, optimizer, learningR
 		batch_num = 0
 		for images, _ in dataloader:
 			batch_num = batch_num + 1
-			if batch_num%100 == 0:
+			if batch_num%200 == 0:
 				print('training data on batch',batch_num)
 			images = images.to(device) # send to GPU if available
 			images_out = model(images)# forward
@@ -242,8 +312,9 @@ def train(device, num_epochs, dataloader, model, criterion, optimizer, learningR
 			best_model_wts = copy.deepcopy(model.state_dict())
 		learningRateScheduler.step()
 
-	time_elapsed = time.time() - since
-	print( 'Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+		time_elapsed = time.time() - since
+		print( 'epoch complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+	#--------------------end of epoch loop--------------------------------------------------------
 	print( 'Best training loss: {:4f}'.format(best_loss))
 
 	# load best model weights
@@ -256,6 +327,7 @@ def inference(device, image, model):
 	"""
 	model = model.to(device)
 	image = image.to(device)
+	model.eval() # don't cache the intermediate values
 	image_out = model(image)
 	return image_out
 
@@ -266,5 +338,6 @@ def encode(device, image, model):
 	"""
 	model = model.to(device)
 	image = image.to(device)
+	model.eval() # don't cache the intermediate values
 	features = model.encode(image)
 	return features
