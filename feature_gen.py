cc1e993e5b5e1183de31c7cf77ea801a6b4605b1
Wentao Shi <wentao771@gmail.com>
2019-04-07 13:53:03 -0500

    added feature_gen.py, not tested

--- feature_gen.py
+++ feature_gen.py
@@ -5,12 +5,23 @@ This code is used to visually verify the correctness of the autoencoder
 """
 import torch
 import argparse
+import pickle
+import numpy as np
+import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
-from utils import DenseAutoencoder, inference
+from torch import nn
+from os import listdir
+from os.path import isfile, join
+from utils import UnsuperviseDataset, DenseAutoencoder, ConvAutoencoder, encode
 from helper import imshow
 
 def getArgs():
     parser = argparse.ArgumentParser('python')
+    parser.add_argument('-index',
+                        default=0,
+                        type=int,
+                        required=False,
+                        help='index of image')
     parser.add_argument('-data_dir',
                         default='../bae-data-images/',
                         required=False,
@@ -18,50 +29,83 @@ def getArgs():
     parser.add_argument('-feature_dir',
                         default='../bae-data-features/',
                         required=False,
-                        help='directory of encoded')                        
-    parser.add_argument('-model_file',
-                        default='../trained_model/bionoi_autoencoder_dense.pt',
+                        help='directory of generated features')
+    parser.add_argument('-style',
+                        default='conv',
                         required=False,
-                        help='file to save the model')
+                        choices=['conv', 'dense'],
+                        help='style of autoencoder')                        
+    parser.add_argument('-normalize',
+                        default=True,
+                        required=False,
+                        help='whether to normalize dataset')                       
     return parser.parse_args()
 
+def feature_vec_gen(device, model, dataset, feature_dir):
+    """
+    Generate feature vectors for a single image
+    """
+
+    m = len(dataset)
+    for i in range(m)
+        image, file_name = dataset[i]
+        file_name = file_name.split('.')
+        file_name = file_name[0]
+        feature_vec = encode(device, image, model)
+        pickle_file = open(feature_dir + file_name + '.pickle','wb')
+        pickle.dump(feature_vec, pklFile)
 
 if __name__ == "__main__":
     args = getArgs()
     index = args.index
     data_dir = args.data_dir
-    model_file = args.model_file
+    feature_dir = args.feature_dir
+    style = args.style
+    normalize = args.normalize
 
- 	# Detect if we have a GPU available
-	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-	print('Current device: '+str(device))   
+    # Detect if we have a GPU available
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print('Current device: '+str(device))   
 
-    transform = transforms.Compose([transforms.ToTensor()])
+    if style == 'conv':
+        model_file = '../trained_model/bionoi_autoencoder_conv.pt'
+    elif style == 'dense':
+        model_file = '../trained_model/bionoi_autoencoder_dense.pt'
 
-	# put images into dataset
-	img_list = [f for f in listdir(data_dir) if isfile(join(data_dir, f))]
-	dataset = UnsuperviseDataset(data_dir, img_list, transform=transform)  
+    data_mean = [0.6150, 0.4381, 0.6450]
+    data_std = [0.6150, 0.4381, 0.6450]   
+    if normalize == True:
+        print('normalizing data:')
+        print('mean:', data_mean)
+        print('std:', data_std)
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize((data_mean[0], data_mean[1], data_mean[2]),
+                                                             (data_std[0], data_std[1], data_std[2]))])
+    else:
+        transform = transforms.Compose([transforms.ToTensor()])
+	
+    # put images into dataset
+    img_list = [f for f in listdir(data_dir) if isfile(join(data_dir, f))]
+    dataset = UnsuperviseDataset(data_dir, img_list, transform=transform)  
+    image, file_name = dataset[index]
+    # calulate the input size (flattened)
+    print('name of input:', file_name)
+    image_shape = image.shape
+    print('shape of input:', image_shape)
+    input_size = image_shape[0]*image_shape[1]*image_shape[2]
+    print('flattened input size:',input_size) 
 
-    	# create dataloader
-	dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=1)
-
-    # instantiate model
-	model = DenseAutoencoder(input_size, feature_size)
+    # instantiate and load model
+    if style == 'conv':
+        model = ConvAutoencoder()
+    elif style == 'dense':
+        model = DenseAutoencoder(input_size, feature_size)  
+ 
     # if there are multiple GPUs, split the batch to different GPUs
-	if torch.cuda.device_count() > 1:
-		print("Using "+str(torch.cuda.device_count())+" GPUs...")
-		model = nn.DataParallel(model)
-    # load model
-	model.load_state_dict(torch.load(model_file))
+    if torch.cuda.device_count() > 1:
+        print("Using "+str(torch.cuda.device_count())+" GPUs...")
+        model = nn.DataParallel(model)
+    model.load_state_dict(torch.load(model_file))
 
-    # get the reconstructed image
-    reconstruct_image = inference(device, image, model)
-
-    fig, (ax1, ax2) = plt.subplots(nrows=1,ncols=2,figsize=(14, 7))
-    ax1.imshow(image)
-    ax1.set_title('Original Image')
-    ax2.imshow(reconstruct_image)
-    ax2.set_title('Reconstructed Image')
-    # show both figures
-    #plt.savefig('./images/'+str(opMode)+'_'+str(imgClass)+str(index)+'.png')
-    plt.show()
+    # generate features for images in data_dir
+    feature_vec_gen(device, model, dataset, feature_dir)
